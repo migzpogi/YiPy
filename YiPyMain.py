ddafa1032a846790f6023ef8261d1d15d2d07f03
Migz Estrella <migz.zgim@gmail.com>
2017-10-18 23:30:09 +0800

    Added Movie class

--- YiPyMain.py
+++ YiPyMain.py
@@ -1,1 +1,57 @@
-print("Hello World")
+import feedparser
+import re
+
+class Movie(object):
+    """
+    Object representation of a movie entry
+    """
+
+    def __init__(self, movie):
+        self.movie = movie
+        self.__set_information()
+
+    def __set_information(self):
+        """
+        Sets the various information of the movie such as title, ratings, etc.
+        :return:
+        """
+
+        # title
+        self.title = self.movie.title
+
+        # quality
+        if '[720p]' in self.title:
+            self.quality = '720p'
+        elif '[1080p]' in self.title:
+            self.quality = '1080p'
+        else:
+            self.quality = None
+
+        # summary
+        self.summary = self.movie.summary
+
+        # imdb rating
+        search = re.search(r'IMDB Rating: [0-9].[0-9][/]10', self.movie.summary)
+        self.imdb = search.group()
+
+        # synopsis
+        parts = self.summary.split('>')  # split the summary part with '>' as the delimiter
+        last_part = parts[len(parts)-1]  # get only the last part
+        self.synopsis = last_part
+
+        # download link
+        self.downloadlink = self.movie.links[1]['href']
+
+
+def load_rss(rss_feed):
+    """
+    Reads the RSS feed of YTS
+    :param rss_feed:
+    :return:
+    """
+
+    return feedparser.parse(rss_feed)
+
+
+if __name__ == '__main__':
+    ytsRSS = load_rss('https://yts.ag/rss')
