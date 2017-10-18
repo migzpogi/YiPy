7f03f13e9bbc2ceaa13b6bcf850f9180f69e3805
Migz Estrella <migz.zgim@gmail.com>
2017-10-18 23:29:53 +0800

    Added unit test

---
+++ tests/testYipy.py
@@ -0,0 +1,13 @@
+import unittest
+from YiPyMain import load_rss
+
+
+class TestYipyMain(unittest.TestCase):
+
+    def testLoadRSS(self):
+        yts_rss = load_rss('sample.xml')
+        self.assertEqual(yts_rss.feed.title, 'YTS RSS')
+
+
+if __name__ == '__main__':
+    unittest.main()
