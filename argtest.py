238629760a184f5f65d292785d81765182237559
Migz Estrella <miguel_estrella@trendmicro.com>
2017-11-24 15:15:29 +0800

    Modified argtest.py for a skeleton of mode picker

--- argtest.py
+++ argtest.py
@@ -1,1 +1,31 @@
-print("hello world")
+import argparse
+import sys
+
+def determine_modes(args):
+    """
+
+    :param args:
+    :return:
+    """
+
+    print args.console_status
+    print args.file_status
+    print args.email_status
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Please choose from the following modes:")
+
+    parser.add_argument('--console', '-c', default=False, action='store_true',
+                        dest='console_status', help='Shows the output in the console.')
+    parser.add_argument('--file', '-f', default=False, action='store_true',
+                        dest='file_status', help='Writes the output in a text file.')
+    parser.add_argument('--email', '-e', default=False, action='store_true',
+                        dest='email_status', help='Sends the output to an email.')
+
+    if len(sys.argv) > 1:
+        args = parser.parse_args()
+        determine_modes(args)
+    else:
+        parser.print_help()
+        parser.exit()
