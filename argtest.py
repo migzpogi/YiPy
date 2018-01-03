c57c15a8da243312e54915a8a7a5bfbbb93b4544
Migz Estrella <miguel_estrella@trendmicro.com>
2018-01-03 14:25:21 +0800

    Console mode running in argtest.py

--- argtest.py
+++ argtest.py
@@ -3,28 +3,68 @@ import sys
 import logging.config
 from ConfigParser import ConfigParser
 import feedparser
+import re
 
-def run_commands(args):
+
+class Config(object):
     """
-    Runs the application with the specified commands
-    :param args:
-    :return:
+    Object representation of the config file
     """
-    
-    # load config
-    config = ConfigParser()
-    config.read('./config/settings.ini')
-    log.info('Config loaded. Parsing the RSS feed...')
 
-    # load the rss feed
-    ytsRSS = load_rss('https://yts.ag/rss')
+    def __init__(self, config):
+        self.config = config
+        self.smtpuser = self.config.get('smtp', 'username')
+        self.smtppass = self.config.get('smtp', 'password')
+        self.emailfrom = self.config.get('email', 'from')
+        self.emailto = self.config.get('email', 'to')
+        self.emailsubject = self.config.get('email', 'subject')
 
-    if args.console_status:
-        print "Console Mode"
-    if args.file_status:
-        print "File Mode"
-    if args.email_status:
-        print "Email mode"
+
+class Movie(object):
+    """
+    Object representation of a movie entry
+    """
+
+    def __init__(self, movie):
+        self.movie = movie
+        self.__set_information()
+
+    def __set_information(self):
+        """
+        Sets the various information of the movie such as title, ratings, etc.
+        :return:
+        """
+
+        # title
+        self.title = self.movie.title
+
+        # clean title: strips the quality part section of the title
+        self.cleantitle = self.title[:-8]
+
+        # quality
+        if '[720p]' in self.title:
+            self.quality = '720p'
+        elif '[1080p]' in self.title:
+            self.quality = '1080p'
+        elif '[3D]' in self.title:
+            self.quality = '3D'
+        else:
+            self.quality = None
+
+        # summary
+        self.summary = self.movie.summary
+
+        # imdb rating
+        search = re.search(r'IMDB Rating: [0-9].[0-9][/]10', self.movie.summary)
+        self.imdb = search.group()
+
+        # synopsis
+        parts = self.summary.split('>')  # split the summary part with '>' as the delimiter
+        last_part = parts[len(parts)-1]  # get only the last part
+        self.synopsis = last_part
+
+        # download link
+        self.downloadlink = self.movie.links[1]['href']
 
 
 def print_ascii():
@@ -43,6 +83,7 @@ def print_ascii():
   Version 1.0                         
     """)
 
+
 def load_rss(rss_feed):
     """
     Reads the RSS feed of YTS
@@ -53,10 +94,82 @@ def load_rss(rss_feed):
 
     try:
         feed = feedparser.parse(rss_feed)
+        log.info('RSS feed parsed!')
     except Exception as e:
         log.warn(e)
 
     return feed
+
+
+def generate_movie_list(yts_rss):
+    """
+    Given a parsed YTS RSS, this function will generate a list of movies using the Movie class
+    :param yts_rss:
+    :return: List of Movie objects
+    """
+
+    movie_list = []
+    for idx, m in enumerate(yts_rss.entries):
+        movie_list.append(Movie(m))
+
+    return movie_list
+
+
+def filter_movie_list(movie_list, quality='1080p'):
+    """
+    Filters the given movie list according to the passed parameters
+    :param quality:
+    :return:
+    """
+
+    filtered_list = []
+    for idx, m in enumerate(movie_list):
+        if m.quality == quality:
+            filtered_list.append(m)
+
+    return filtered_list
+
+
+def mode_console(parsed_rss):
+    """
+
+    :param parsed_rss:
+    :return:
+    """
+
+    log.info('Running YiPy in Console Mode')
+    log.info('***** RECENT UPLOADS START *****')
+
+    movie_list = filter_movie_list(generate_movie_list(parsed_rss))
+
+    for movies in movie_list:
+        log.info(movies.cleantitle)
+
+    log.info('***** RECENT UPLOADS END *****')
+
+
+def run_commands(args):
+    """
+    Runs the application with the specified commands
+    :param args:
+    :return:
+    """
+
+    # load config
+    config = ConfigParser()
+    config.read('./config/settings.ini')
+    log.info('Config loaded. Parsing the RSS feed...')
+
+    # load the rss feed
+    ytsRSS = load_rss(config.get('rssfeed', 'rss'))
+
+    if args.console_status:
+        mode_console(ytsRSS)
+    if args.file_status:
+        print "File Mode"
+    if args.email_status:
+        print "Email mode"
+        
 
 if __name__ == '__main__':
     # initialize logging
