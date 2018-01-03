217c68ff66e3998169c7dc27abda60f0333a7d45
Migz Estrella <miguel_estrella@trendmicro.com>
2018-01-03 13:47:40 +0800

    Added load_rss() method in argtest.py. Renamed determine_modes() to run_commands()

--- argtest.py
+++ argtest.py
@@ -1,19 +1,29 @@
 import argparse
 import sys
+import logging.config
+from ConfigParser import ConfigParser
+import feedparser
 
-def determine_modes(args):
+def run_commands(args):
     """
-
+    Runs the application with the specified commands
     :param args:
     :return:
     """
 
-    print args.console_status
-    print args.file_status
-    print args.email_status
+    if args.console_status:
+        print "Console Mode"
+    if args.file_status:
+        print "File Mode"
+    if args.email_status:
+        print "Email mode"
 
 
 def print_ascii():
+    """
+    Prints an ASCII art of the project name
+    :return:
+    """
     print("""
  _     _ _____ ______  _     _ 
 | |   | (_____|_____ \| |   | |
@@ -25,7 +35,38 @@ def print_ascii():
   Version 1.0                         
     """)
 
+def load_rss(rss_feed):
+    """
+    Reads the RSS feed of YTS
+    :param rss_feed:
+    :return: feedparser.FeedParserDict
+    """
+    feed = None
+
+    try:
+        feed = feedparser.parse(rss_feed)
+    except Exception as e:
+        log.warn(e)
+
+    return feed
+
 if __name__ == '__main__':
+    # initialize logging
+    log = logging.getLogger("YiPy")
+    logging.config.fileConfig(
+        disable_existing_loggers=False,
+        fname='./log/logconfig.ini',
+        defaults={'logfilename': './log/yipytrace.log'})
+    log.info('YiPy started. Reading config...')
+
+    # load config
+    config = ConfigParser()
+    config.read('./config/settings.ini')
+    log.info('Config loaded. Parsing the RSS feed...')
+
+    # load the rss feed
+    ytsRSS = load_rss('https://yts.ag/rss')
+
     parser = argparse.ArgumentParser(
         description="Please choose from the following modes:")
 
@@ -38,8 +79,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) > 1:
         args = parser.parse_args()
-        determine_modes(args)
-        print_ascii()
+        run_commands(args)
 
     else:
         parser.print_help()
